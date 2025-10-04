accf1c5225230e8e5d1fa80165ed59fcc70aae66
anad-zeal <elza.dana@gmail.com>
2025-10-04 12:49:33 -0600

    Fix wording in 'Restoration Projects' description in home.php

--- pages/home.php
+++ pages/home.php
@@ -5,8 +5,7 @@
 
     <p>
       <a href="/restoration?showSlideshow=true" class="dropcap" data-gallery="restoration" aria-label="Go to Restoration Projects">R</a>
-      estoration Projects —Almost instantaneous astral travel to any point in their universe is now possible via the implementation of hyperbolic sub-resonant variations, combined with isomorphic silicon overlays manifesting dolor sit abet connecter advising elia.
-    </p>
+      estoration Projects —Almost instantaneous astral travel to any point in our
 
     <p>
       <a href="/decorative?showSlideshow=true" class="dropcap" data-gallery="decorative" aria-label="Go to Decorative Painting">D</a>

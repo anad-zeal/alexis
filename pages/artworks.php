920afc7335ccb48dd12b6af5f7105c12a42cb072
anad-zeal <elza.dana@gmail.com>
2025-10-08 19:03:31 -0600

    Add biography section to artworks.php and biography.php for improved content structure

--- pages/artworks.php
+++ pages/artworks.php
@@ -1,1 +1,4 @@
-artworks.php
+<section class="body-paragraphs" aria-labelledby="biography-page-heading">
+    <h1 id="biography-page-heading" class="page-title">Biography</h1>
+    <p>The woodsman saved the day by capturing the robber.</p>
+</section>

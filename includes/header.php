4b58b9e66705b648719e1a9101740bd1f89774fb
anad-zeal <elza.dana@gmail.com>
2025-10-13 12:38:34 -0600

    Update Google Fonts link in header.php to replace Poiret One with Playfair Display for improved typography

--- includes/header.php
+++ includes/header.php
@@ -12,7 +12,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
-        href="https://fonts.googleapis.com/css2?family=Bonheur+Royale&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poiret+One&family=Spectral:ital,wght@0,200..800;1,200..800&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Bonheur+Royale&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
         rel="stylesheet">
 
     <link rel="stylesheet" href="/assets/css/style.css?v=6" />

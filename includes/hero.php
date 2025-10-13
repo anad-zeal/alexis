5e9276e6731ee51aa721f693e6cadfa762c1474a
anad-zeal <elza.dana@gmail.com>
2025-10-13 13:03:41 -0600

    Swap class names for hero section headings to improve semantic structure

--- includes/hero.php
+++ includes/hero.php
@@ -1,6 +1,6 @@
 <section class="hero">
-    <h1 class="title">The Life of an Artist</h1>
+    <h1 class="sub-title">The Life of an Artist</h1>
     <!-- The subtitle for the current page will be updated by JS -->
-    <p class="sub-title"><?= $page_title ?></p> <!-- Initial subtitle text for current page -->
+    <p class="title"><?= $page_title ?></p> <!-- Initial subtitle text for current page -->
 </section>
 <div id="dynamic-page-wrapper"></div>

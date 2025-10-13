583fea1e9bdab6b8e523c1463303242b612445c0
anad-zeal <elza.dana@gmail.com>
2025-10-13 13:06:28 -0600

    Update class name for subtitle paragraph in hero section for improved semantic structure

--- includes/hero.php
+++ includes/hero.php
@@ -1,6 +1,6 @@
 <section class="hero">
     <h1 class="sub-title">The Life of an Artist</h1>
     <!-- The subtitle for the current page will be updated by JS -->
-    <p class="title"><?= $page_title ?></p> <!-- Initial subtitle text for current page -->
+    <p class="page-title"><?= $page_title ?></p> <!-- Initial subtitle text for current page -->
 </section>
 <div id="dynamic-page-wrapper"></div>

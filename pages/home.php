d6bac5e60f99950bb0819ba671426abee5ddacfa
anad-zeal <elza.dana@gmail.com>
2025-10-14 14:37:17 -0600

    Comment out section tags in home.php for cleaner code structure

--- pages/home.php
+++ pages/home.php
@@ -1,18 +1,17 @@
-<section class="body-paragraphs" aria-labelledby="home-page-heading">
+<!-- <section class="body-paragraphs" aria-labelledby="home-page-heading"> -->
 
-    <p>
-        <a href="/restoration" class="category" data-gallery="restoration"
-            aria-label="Go to Restoration Projects">RestorationProjects</a>
-    </p>
+<p>
+    <a href="/restoration" class="category" data-gallery="restoration"
+        aria-label="Go to Restoration Projects">RestorationProjects</a>
+</p>
 
-    <p>
-        <a href="/black-and-white" class="category" data-gallery="black-and-white"
-            aria-label="Go to Black & White">Black & White</a>
-    </p>
+<p>
+    <a href="/black-and-white" class="category" data-gallery="black-and-white" aria-label="Go to Black & White">Black &
+        White</a>
+</p>
 
-    <p>
-        <a href="/artworks" class="category" data-gallery="artworks"
-            aria-label="Go to Artworks">Artworks</a>
-    </p>
+<p>
+    <a href="/artworks" class="category" data-gallery="artworks" aria-label="Go to Artworks">Artworks</a>
+</p>
 
-</section>
+<!-- </section> -->

3b9853032f71bc261a1d076e9532213f1b72f926
anad-zeal <elza.dana@gmail.com>
2025-10-14 06:34:33 -0600

    Refactor home.php to update link classes for consistency

--- pages/home.php
+++ pages/home.php
@@ -1,18 +1,18 @@
 <section class="body-paragraphs" aria-labelledby="home-page-heading">
 
     <p>
-        <a href="/restoration.php" class="dropcap" data-gallery="restoration"
+        <a href="/restoration.php" class="images" data-gallery="restoration"
             aria-label="Go to Restoration Projects">R</a>estoration
         Projects —
     </p>
 
     <p>
-        <a href="/decorative.php" class="dropcap" data-gallery="decorative" aria-label="Go to Decorative Painting">D</>
-            ecorative Painting
+        <a href="/decorative.php" class="images" data-gallery="decorative"
+            aria-label="Go to Decorative Painting">D</a>+++ecorative Painting
     </p>
 
     <p>
-        <a href="/artworks.php?showSlideshow=true" class="dropcap" data-gallery="artworks"
+        <a href="/artworks.php?showSlideshow=true" class="images" data-gallery="artworks"
             aria-label="Go to Artworks">A</a>rtworks
     </p>
 

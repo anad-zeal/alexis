364540c4090ff8b822dbc4c0ed8779eb2dc0a05b
anad-zeal <elza.dana@gmail.com>
2025-10-14 16:40:49 -0600

    Update slideshow data sources and add project series section

--- pages/encaustic.php
+++ pages/encaustic.php
@@ -1,4 +1,4 @@
 <section class="slideshow-section">
-    <div class="slideshow-container" data-slides="/js/encaustic-slides.json" data-autoplay="true" data-interval="5000"
-        data-fade="1500"></div>
+    <div class="slideshow-container" data-slides="/js/encaustic-paintings.json" data-autoplay="true"
+        data-interval="5000" data-fade="1500"></div>
 </section>
---
+++ pages/project-series.php
@@ -0,0 +1,4 @@
+<section class="slideshow-section">
+    <div class="slideshow-container" data-slides="/json-files/project-series.json" data-autoplay="true"
+        data-interval="5000" data-fade="1500"></div>
+</section>

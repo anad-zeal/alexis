499214a27fc454168da556dbebbe6a0f2030400d
anad-zeal <elza.dana@gmail.com>
2025-10-15 09:13:09 -0600

    Add category class to artwork links for improved styling consistency

--- pages/artworks.php
+++ pages/artworks.php
@@ -2,21 +2,21 @@
 <h2 id="artworks-heading" class="visually-hidden">Artworks</h2>
 
 <p>
-    <a href="/black-and-white" class="images" data-gallery="black_and_white_paintings"
+    <a href="/black-and-white" class="category images" data-gallery="black_and_white_paintings"
         aria-label="Go to Black and White Paintings">Black and White Paintings</a>
 </p>
 
-<p><a href="/decorative" class="images" data-gallery="decorative_painting"
+<p><a href="/decorative" class="category images" data-gallery="decorative_painting"
         aria-label="Go to Decorative Painting">Decorative Painting</a>
 </p>
 
 <p>
-    <a href="/encaustic" class="images" data-gallery="encaustic_paintings"
+    <a href="/encaustic" class="category images" data-gallery="encaustic_paintings"
         aria-label="Go to Encaustic Paintings">Encaustic Paintings</a>
 </p>
 
 <p>
-    <a href="/projects" class="images" data-gallery="project_series_paintings"
+    <a href="/projects" class="category images" data-gallery="project_series_paintings"
         aria-label="Go to Project Series Paintings">Project Series Paintings</a>
 </p>
 <!-- </section> -->

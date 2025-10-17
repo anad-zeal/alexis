c919b90211bf5b6e72bdcc73b394998a2b90d9c2
anad-zeal <elza.dana@gmail.com>
2025-10-16 19:55:48 -0600

    Improve formatting and structure of home.php for better readability

--- pages/home.php
+++ pages/home.php
@@ -1,15 +1,20 @@
 <div class="container">
+
     <div class="restoration-projects">
         <a href="/restoration" class="category" data-gallery="restoration"
             aria-label="Go to Restoration Projects">Restoration Projects</a>
     </div>
-    <div class="center-blank"></div>
+
+    <div class="center-blank">&nbsp;</div>
+
     <div class="decorative-painting">
-        <a href="/decorative" class="category" data-gallery="decorative" aria-label="Go to Decorative Painting">
-            Decorative Painting</a>
+        <a href="/decorative" class="category" data-gallery="decorative"
+            aria-label="Go to Decorative Painting">Decorative Painting</a>
     </div>
+
     <div class="artworks">
         <a href="/artworks" class="category" data-gallery="artworks" aria-label="Go to Artworks">Artworks</a>
 
     </div>
+
 </div>

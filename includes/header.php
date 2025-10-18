dacd0c79dbbb0383475fb41d4b577cd2235c50eb
anad-zeal <elza.dana@gmail.com>
2025-10-18 07:58:00 -0600

    Update brand color to RGB format and change font family to Inter for improved consistency

--- includes/header.php
+++ includes/header.php
@@ -72,7 +72,7 @@ function nav_item(string $slug, string $label, string $href): string
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
-        href="https://fonts.googleapis.com/css2?family=Bonheur+Royale&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Bonheur+Royale&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap"
         rel="stylesheet">
 
     <!-- Main Stylesheet -->

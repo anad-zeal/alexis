f52c1ec7dca59994b0501086d165e30de3d47740
anad-zeal <elza.dana@gmail.com>
2025-10-23 17:50:10 -0600

    Add top-grid layout for improved header structure and styling

--- includes/hero.php
+++ includes/hero.php
@@ -4,13 +4,15 @@
 
 </section>
 <div id="main-content-area">
-    <div class="ss-header">
-        <div class="aep-logo">
+    <div class="top-grid">
+        <div class="left aep-logo">
             <h2 class="sub-title">The Life of an Artist</h2>
         </div>
-        <div class="ss-category">
+
+        <div class="right ss-category">
             <p class="page-title"></p>
         </div>
     </div>
-    <!-- Initial page content will be inserted here by index.php -->
-    <!-- Dynamic content will be loaded here by navigation.js -->
+</div>
+<!-- Initial page content will be inserted here by index.php -->
+<!-- Dynamic content will be loaded here by navigation.js -->

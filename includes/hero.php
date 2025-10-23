ea64c3a22200bb1b22db33c7ad2c6a6b592e0756
anad-zeal <elza.dana@gmail.com>
2025-10-23 17:53:51 -0600

    Refactor hero and project-series sections to unify layout structure and improve styling

--- includes/hero.php
+++ includes/hero.php
@@ -3,16 +3,6 @@
     <p class="page-title"></p>
 
 </section>
-<div id="main-content-area">
-    <div class="top-grid">
-        <div class="left aep-logo">
-            <h2 class="sub-title">The Life of an Artist</h2>
-        </div>
 
-        <div class="right ss-category">
-            <p class="page-title"></p>
-        </div>
-    </div>
-</div>
 <!-- Initial page content will be inserted here by index.php -->
 <!-- Dynamic content will be loaded here by navigation.js -->

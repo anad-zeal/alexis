6553c0b69872a7f26e53b36b26be2b5d9d6c1e12
anad-zeal <elza.dana@gmail.com>
2025-10-27 14:42:07 -0600

    Refactor index.php: Fix indentation for require statements to improve code readability

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 
 $file = "includes/home.php";
 
-    require __DIR__ . "/includes/header.php";
-    require __DIR__ . "/includes/hero.php";
-    require __DIR__ . "/$file";
-    require __DIR__ . "/includes/footer.php";
-}
+require __DIR__ . "/includes/header.php";
+require __DIR__ . "/includes/hero.php";
+require __DIR__ . "/$file";
+require __DIR__ . "/includes/footer.php";

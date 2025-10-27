04c94efad8ad6d9510f9b513a5ebb14c7ef685d2
anad-zeal <elza.dana@gmail.com>
2025-10-27 14:36:48 -0600

    Refactor footer.php: Replace slideshow.js and navigation.js with script.js for streamlined script management

--- includes/footer.php
+++ includes/footer.php
@@ -7,8 +7,7 @@
     <p>&copy; 2025 elzalive • All rights reserved.</p>
 </footer>
 
-<script src="/assets/js/slideshow.js"></script>
-<script src="/assets/js/navigation.js"></script>
+<script src="/assets/js/script.js"></script>
 </body>
 
 </html>

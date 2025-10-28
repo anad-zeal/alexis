44798ffa995bff2b2df3a500f76d07800019ff6e
anad-zeal <elza.dana@gmail.com>
2025-10-27 18:09:33 -0600

    Refactor index.php: Replace duplicate footer inclusion with hero inclusion for code clarity

--- index.php
+++ index.php
@@ -4,4 +4,4 @@
 require __DIR__ . "/includes/header.php";
 require __DIR__ . "/includes/hero.php";
 require __DIR__ . $json_file;
-require __DIR__ . "/includes/footer.php";
+require __DIR__ . "/includes/hero.php";

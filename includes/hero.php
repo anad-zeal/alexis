622080596f370535888868a75596c5eb7654bbec
anad-zeal <elza.dana@gmail.com>
2025-10-28 22:16:38 -0600

    Fix hero.php: Update main element data-page attribute to a static value and restructure dynamic content area for clarity

--- includes/hero.php
+++ includes/hero.php
@@ -3,5 +3,8 @@
      <p class="page-title"></p>
  </section>
 
- <main id="dynamic-page-wrapper" data-page="<?= htmlspecialchars($active_page) ?>" tabindex="-1">
+ <main id="dynamic-page-wrapper" data-page="home" tabindex="-1">
+     <div id="dynamic-content-area">
+         <!-- Dynamic content will be loaded here by script.js -->
+     </div>
  </main>
